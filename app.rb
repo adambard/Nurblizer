5cefe700cd1e04fa2ce41e7737cef5b15da51e9a
Adam <adam@adambard.com>
2013-03-25 18:36:53 -0700

    Whoops

--- app.rb
+++ app.rb
@@ -1,10 +1,10 @@
 require 'sinatra'
 
+# configure block is a Sinatra feature
 configure do
     @@nouns = File.open('nouns.txt').map{|line|
         line.strip.downcase
     }
-
 end
 
 
@@ -14,17 +14,22 @@ def nurble(text)
 
     words.each{|w|
         if not @@nouns.include? w
-            text.gsub! Regexp.new('(\b)'+ w + '(\b)', Regexp::IGNORECASE), "\1<span class=\"nurble\">nurble</span>\2"
+          pattern = Regexp.new('(\b)'+ w + '(\b)', Regexp::IGNORECASE)
+          replacement = "\1<span class=\"nurble\">nurble</span>\2"
+          text.gsub! pattern, replacement
         end
     }
     text.gsub(/\n/, '<br>')
 end
 
+
 get "/" do
     haml :index
 end
 
-post "/nurble" do
-    haml :nurble, :locals => {:text => nurble(params["text"])}
-end
 
+post "/nurble" do
+    haml :nurble, :locals => {
+      :text => nurble(params["text"])
+    }
+end
